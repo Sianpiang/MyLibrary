d81f88e46b66e5e79b63a7fbac0198862cfe134a
DavidThangsianpiang <sianpiang8@gmail.com>
2022-05-04 18:54:38 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myList = document.querySelector("#myList");
+const myList = document.querySelector(".myList");
 
 
 const MyLibrary = [
@@ -33,7 +33,7 @@ function addBooks(book){
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.pageNo}</td>
-    <td><button>${book.haveread}<button></td>
+    <td><button class = status>${book.haveread}<button></td>
     <td><button class = delete>Del</button></td>
     `;
     myList.appendChild(row);
@@ -43,6 +43,21 @@ function clearField(){
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
     document.querySelector("#pageNo").value = "";
+}
+
+function deleteBook(ele){
+    if(ele.classList.contains('delete')){
+        ele.parentElement.parentElement.remove();
+    }
+}
+
+function bookStatus(book){
+    if(MyLibrary[book].haveread === "Read"){
+        MyLibrary[book].haveread = "Not Read";
+    }
+    else{
+        MyLibrary[book].haveread = "Read"; 
+    }
 }
 
 //Displaying the Books
@@ -57,8 +72,23 @@ document.querySelector("#form-bolte").addEventListener('submit',(e)=>{
     const haveread = document.querySelector("#haveread").value;
 
     const book = new Books(title,author,pageNo,haveread);
+    
+    //Adding the books
     addBooks(book);
+
+    //Clearing the field
     clearField();
 })
+
+//Delete function
+
+document.querySelector(".myList").addEventListener('click',(e)=>{
+    deleteBook(e.target);
+})
+
+document.querySelector(".myList").addEventListener('click',(e)=>{
+    bookStatus(e.target);
+})
+
 
 
